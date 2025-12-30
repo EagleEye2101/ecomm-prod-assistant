d92d4a874fda233c3044f454772c2727b65c2a76
EagleEye2101 <karan.raka2003@gmail.com>
2025-12-29 23:25:00 -0500

    test

--- prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py
+++ prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py
@@ -29,17 +29,17 @@ class AgenticRAG:
         # Initialize MCP client
         self.mcp_client = MultiServerMCPClient(
             {
-                # "hybrid_search": {
-                #     "transport": "streamable_http",
-                #     "url": "http://localhost:8000/mcp"
-                # }
                 "hybrid_search": {
-                    "command": "python",
-                    "args": ["./prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py"],
-                    #"port": 8000,
-                    #"transport": "streamable_http"
-                    "transport": "stdio"
+                    "transport": "streamable_http",
+                    "url": "http://localhost:8000/mcp"
                 }
+                # "hybrid_search": {
+                #     "command": "python",
+                #     "args": ["./prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py"],
+                #     #"port": 8000,
+                #     #"transport": "streamable_http"
+                #     "transport": "stdio"
+                # }
             }
         )
 
